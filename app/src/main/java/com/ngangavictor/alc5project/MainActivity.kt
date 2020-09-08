f9c03ef8f761a1e0339e252a523e9f831a83eb1b
ngangavic <ngangavictor10@gmail.com>
2020-09-08 17:55:08 +0300

    app launcher

--- app/src/main/java/com/ngangavictor/alc5project/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/alc5project/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.ngangavictor.alc5project
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.ngangavictor.alc5project.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme_NoActionBar)
         setContentView(R.layout.activity_main)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
