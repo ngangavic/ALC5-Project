cdd6a99b1c28bb8f2fc376deaec3e75c45bdaab7
ngangavic <ngangavictor10@gmail.com>
2020-09-10 08:55:17 +0300

    open submit activity

--- app/src/main/java/com/ngangavictor/alc5project/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/alc5project/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ngangavictor.alc5project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.tabs.TabLayout
 import com.ngangavictor.alc5project.ui.main.SectionsPagerAdapter
 
@@ -17,6 +20,10 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+        val appBarLayout:AppBarLayout=findViewById(R.id.appBarLayout)
+        appBarLayout.findViewById<Button>(R.id.action_button_submit).setOnClickListener {
+            startActivity(Intent(this,SubmitActivity::class.java))
+        }
 
     }
 }
