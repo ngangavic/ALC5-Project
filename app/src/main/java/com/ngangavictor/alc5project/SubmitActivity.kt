4157048795ce4b1af8a92b615146e3d70b31e932
ngangavic <ngangavictor10@gmail.com>
2020-09-10 09:19:54 +0300

    submit activity set up

--- app/src/main/java/com/ngangavictor/alc5project/SubmitActivity.kt
+++ app/src/main/java/com/ngangavictor/alc5project/SubmitActivity.kt
@@ -3,12 +3,21 @@ package com.ngangavictor.alc5project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 
 class SubmitActivity : AppCompatActivity() {
 
     private lateinit var imageViewBack: ImageView
+
+    private lateinit var editTextFName:EditText
+    private lateinit var editTextLName:EditText
+    private lateinit var editTextEmail:EditText
+    private lateinit var editTextGithubLink:EditText
+
+    private lateinit var buttonSubmit:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,13 @@ class SubmitActivity : AppCompatActivity() {
         supportActionBar?.elevation = 0F
         val view = supportActionBar?.customView
         imageViewBack = view!!.findViewById(R.id.imageViewBack)
+
+        editTextFName=findViewById(R.id.editTextFName)
+        editTextLName=findViewById(R.id.editTextLName)
+        editTextEmail=findViewById(R.id.editTextEmail)
+        editTextGithubLink=findViewById(R.id.editTextGithubLink)
+
+        buttonSubmit=findViewById(R.id.buttonSubmit)
 
         imageViewBack.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
